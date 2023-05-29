50e179f7e4519f404ed14ff9064a4468fe882093
Kyle burns <datasskyle@gmail.com>
2023-05-29 15:49:00 +0100

    Clerk Login System

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts'
 
 import Date from '../components/date';
+import { SignIn, SignedIn, SignedOut } from '@clerk/nextjs';
 
 export async function getAllPosts() {
   const allPostsData = getSortedPostsData();
@@ -19,31 +20,33 @@ export default async function Home() {
   console.log(allPostsData);
 
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>I'm Kyle</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+    <>
+      <SignedIn>
+        <section className={utilStyles.headingMd}>
+          <p>I'm Kyle</p>
+          <p>
+            (This is a sample website - you’ll be building a site like this on{' '}
+            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          </p>
+        </section>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>{title}</Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </SignedIn>
+      <SignedOut>
+        <SignIn />
+      </SignedOut>
+    </>
   );
 }
--- app/posts/[slug]/page.js
+++ app/posts/[slug]/page.js
@@ -1,26 +1,21 @@
-import Head from "next/head";
-import Layout from "../../../components/layout";
-
 import { getPostData } from '../../../lib/posts';
 import Date from "../../../components/date";
 
 import utilStyles from '../../../styles/utils.module.css';
+import { SignedIn, SignedOut } from '@clerk/nextjs';
 
 export default async function Page({ params }) {
     const postData = await getPostData(params.slug);
 
     return (
-        <Layout>
-            <Head>
-                <title>{postData.title}</title>
-            </Head>
-            <article>
-                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-                <div className={utilStyles.lightText}>
-                    <Date dateString={postData.date} />
-                </div>
-                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-            </article>
-        </Layout>
+        <article>
+            <SignedIn> Hello user </SignedIn>
+            <SignedOut> oh noo, you should sign in!!! </SignedOut>
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <div className={utilStyles.lightText}>
+                <Date dateString={postData.date} />
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
     );
 }
